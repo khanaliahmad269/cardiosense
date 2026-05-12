1b54540001fc88d21acaefb9d22da4f7da8515cb
Ali Ahmad Khan <150443989+khanaliahmad269@users.noreply.github.com>
2026-05-12 14:50:33 +0500

    Add frontend/src/pages/Home.jsx

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import OverviewSection from '../components/OverviewSection';
+
+function Home() {
+  return (
+    <div className="container mx-auto p-4">
+      <OverviewSection />
+    </div>
+  );
+}
+
+export default Home;
