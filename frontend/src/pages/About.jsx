e1f379a3a99b9eac6e3d3b93807dad5285ae69a3
Ali Ahmad Khan <150443989+khanaliahmad269@users.noreply.github.com>
2026-05-12 14:50:41 +0500

    Add frontend/src/pages/About.jsx

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TeamSection from '../components/TeamSection';
+import ModelInfo from '../components/ModelInfo';
+
+function About() {
+  return (
+    <div className="container mx-auto p-4">
+      <TeamSection />
+      <ModelInfo />
+    </div>
+  );
+}
+
+export default About;
